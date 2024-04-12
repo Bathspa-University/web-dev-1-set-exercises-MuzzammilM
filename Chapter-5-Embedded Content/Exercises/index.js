21b668bb9d040bd10bb67cb560abd51a282ed876
MuzzammilM <muzzammilmallick568@gmail.com>
2024-04-12 17:22:17 +0400

    Exercise Chapter 5

--- Chapter-5-Embedded Content/Exercises/index.js
+++ Chapter-5-Embedded Content/Exercises/index.js
@@ -1,8 +1,14 @@
+
+// Select all elements with the class 'sound'
 const sounds = document.querySelectorAll('.sound');
 
+// Iterate over each element with the class 'sound'
 sounds.forEach(sound => {
+    // Add a click event listener to each element
     sound.addEventListener('click', () => {
+        // Create a new Audio object with the source set to the value of the data-sound attribute of the clicked element
         const audio = new Audio(sound.dataset.sound);
+        // Play the audio
         audio.play();
     });
 });
